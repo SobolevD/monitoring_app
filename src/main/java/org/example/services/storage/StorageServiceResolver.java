77d0ed88066e9c48955bb048250257d25fb82343
dmso0321 <dmitrii.sobolev@netcracker.com>
2023-05-01 19:12:06 +0400

    - Added cloud storage clients

--- src/main/java/org/example/services/storage/StorageServiceResolver.java
+++ src/main/java/org/example/services/storage/StorageServiceResolver.java
@@ -1,20 +1,14 @@
 package org.example.services.storage;
 
-import com.netcracker.cloud.dst.exceptions.DataSlicingRuntimeException;
-import com.netcracker.cloud.dst.service.qualifiers.AmazonS3Storage;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.example.services.storage.impl.AmazonS3Storage;
+import org.example.services.storage.impl.AzureBlobStorage;
 import org.example.utils.PropertiesLoader;
-
-import javax.inject.Inject;
 
 import java.util.Properties;
 
-import static com.netcracker.cloud.dst.constants.ApplicationProperties.STORAGE_PROVIDER_PROP;
-import static com.netcracker.cloud.dst.constants.Common.*;
 import static org.example.common.PropertiesNames.STORAGE_PROVIDER_PROP;
-import static org.example.common.StorageProviders.AMAZON_S3;
+import static org.example.common.StorageProviders.*;
 
 @Slf4j
 public class StorageServiceResolver {
@@ -30,11 +24,14 @@ public class StorageServiceResolver {
         String storageProvider = properties.getProperty(STORAGE_PROVIDER_PROP);
 
         switch (storageProvider) {
-            case AMAZON_S3: {
+            case YANDEX_OBJECTS, AMAZON_S3 -> {
                 return new AmazonS3Storage();
             }
-            default: {
-                throw new RuntimeException("Unable to resolve storage!");
+            case AZURE_BLOB -> {
+                return new AzureBlobStorage();
+            }
+            default -> {
+                throw new RuntimeException("Unable to resolve storage: " + storageProvider);
             }
         }
     }
